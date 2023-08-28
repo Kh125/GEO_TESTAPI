d1cc4b9f03982509c55aa0e1ea02e3ea2cc01c17
Khant Hmue <khanthmu152uit@gmail.com>
2023-08-29 04:22:28 +0630

    change route

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.listen(3000, () => {
-    console.log("Listing on port 4000!!!");
-  });
+  console.log("Listing on port 3000!!!");
+});
